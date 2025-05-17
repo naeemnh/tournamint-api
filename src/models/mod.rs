b4a63c03eddfaf018788801bf3d1d21d60d3ab81
Naeem Hussain <naeemnh@outlook.com>
2025-05-17 09:26:15 +0400

    add(model): team

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod player;
 pub(crate) mod user;
+pub(crate) mod team;
---
+++ src/models/team.rs
@@ -0,0 +1,47 @@
+use std::fmt::Write;
+
+use chrono::{DateTime, Utc};
+use sea_query::Iden;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, sqlx::FromRow, Serialize, Deserialize)]
+pub struct Team {
+    pub id: Uuid,
+    pub name: String,
+    #[serde(with = "chrono::serfe::ts_milliseconds")]
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewTeam {
+    pub name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EditableTeam {
+    pub name: String,
+}
+
+pub enum TeamIden {
+    Table,
+    Id,
+    Name,
+    CreatedAt,
+}
+
+impl Iden for TeamIden {
+    fn unquoted(&self, s: &mut dyn Write) {
+        write!(
+            s,
+            "{}",
+            match self {
+                TeamIden::Table => "team",
+                TeamIden::Id => "id",
+                TeamIden::Name => "name",
+                TeamIden::CreatedAt => "created_at",
+            }
+        )
+        .unwrap()
+    }
+}
