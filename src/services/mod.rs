cf75e3ff2726c2ffe0a147da96138b747aa070e8
Naeem Hussain <naeemnh@outlook.com>
2025-05-17 09:57:42 +0400

    add(service): team_service

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,2 +1,3 @@
 pub mod player_service;
+pub mod team_service;
 pub mod user_service;
---
+++ src/services/team_service.rs
@@ -0,0 +1,110 @@
+use actix_web::HttpResponse;
+use oauth2::http::StatusCode;
+use uuid::Uuid;
+
+use crate::{
+    config::DbPool,
+    formatters,
+    models::team::{EditableTeam, NewTeam},
+    repositories::team_repository,
+};
+
+pub async fn get_all_teams(pool: &DbPool) -> HttpResponse {
+    match team_repository::find_all(pool).await {
+        Ok(teams) => formatters::success_response(StatusCode::OK, teams, "RETURNED_PLAYERS"),
+        Err(e) => {
+            let error = e.to_string();
+            let error_message = match error.as_str() {
+                err => err,
+            };
+            formatters::error_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                error_message,
+                "UNKNOWN_ERROR",
+            )
+        }
+    }
+}
+
+pub async fn create_team(pool: &DbPool, team_data: NewTeam) -> HttpResponse {
+    match team_repository::create(pool, team_data).await {
+        Ok(team) => formatters::success_response(StatusCode::CREATED, team, "PLAYER_CREATED"),
+        Err(e) => {
+            let error = e.to_string();
+            let error_message = match error.as_str() {
+                err => err,
+            };
+            formatters::error_response(
+                StatusCode::BAD_REQUEST,
+                error_message,
+                "PLAYER_CREATION_ERROR",
+            )
+        }
+    }
+}
+
+pub async fn get_team(pool: &DbPool, id: Uuid) -> HttpResponse {
+    match team_repository::find_by_id(pool, id).await {
+        Ok(Some(team)) => formatters::success_response(StatusCode::OK, team, "PLAYER_FOUND"),
+        Ok(None) => formatters::error_response(
+            StatusCode::NOT_FOUND,
+            "Could not find team",
+            "PLAYER_NOT_FOUND",
+        ),
+        Err(e) => {
+            let error = e.to_string();
+            let error_message = match error.as_str() {
+                err => err,
+            };
+            formatters::error_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                error_message,
+                "UNKNOWN_ERROR",
+            )
+        }
+    }
+}
+
+pub async fn update_team(pool: &DbPool, id: Uuid, team_data: EditableTeam) -> HttpResponse {
+    match team_repository::update(pool, id, team_data).await {
+        Ok(Some(team)) => formatters::success_response(StatusCode::OK, team, "PLAYER_UPDATED"),
+        Ok(None) => formatters::error_response(
+            StatusCode::BAD_REQUEST,
+            "PLAYER_NOT_FOUND",
+            "Could Not Find Team",
+        ),
+        Err(e) => {
+            let error = e.to_string();
+            let error_message = match error.as_str() {
+                err => err,
+            };
+            formatters::error_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                error_message,
+                "UNKNOWN_ERROR",
+            )
+        }
+    }
+}
+
+pub async fn delete_team(pool: &DbPool, id: Uuid) -> HttpResponse {
+    match team_repository::delete(pool, id).await {
+        Ok(Some(team)) => formatters::success_response(StatusCode::OK, team, "PLAYER_DELETED"),
+        Ok(None) => formatters::error_response(
+            StatusCode::BAD_REQUEST,
+            "PLAYER_NOT_FOUND",
+            "Could Not Find Team",
+        ),
+        Err(e) => {
+            let error = e.to_string();
+            let error_message = match error.as_str() {
+                err => err,
+            };
+            formatters::error_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                error_message,
+                "UNKNOWN_ERROR",
+            )
+        }
+    }
+}
