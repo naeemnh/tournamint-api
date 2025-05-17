8794b8d3216e2681a5b57bb9f4739fb047327a0e
Naeem Hussain <naeemnh@outlook.com>
2025-05-17 09:15:07 +0400

    add(migrations): create team_members table

---
+++ migrations/20250516163830_create_team_members_table.up.sql
@@ -0,0 +1,8 @@
+-- Add up migration script here
+CREATE IF NOT EXISTS team_members (
+    team_id UUID REFERENCES teams (id) ON DELETE CASCADE,
+    player_id UUID REFERENCES players (id) ON DELETE CASCADE,
+    is_captain BOOLEAN DEFAULT FALSE,
+    jersey_number INTEGER,
+    joined_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
